d2634356df1b4b80250a6772ade2f9a597ed4835
ez4bruce <ez4bruce@gmail.com>
2024-08-12 15:33:41 +0800

    fix aliyun env

--- engine/apps/mocloud/mocloud_sms.py
+++ engine/apps/mocloud/mocloud_sms.py
@@ -6,7 +6,12 @@ from alibabacloud_tea_openapi import models as open_api_models
 from alibabacloud_dysmsapi20170525 import models as dysmsapi_20170525_models
 from alibabacloud_tea_util import models as util_models
 from alibabacloud_tea_util.client import Client as UtilClient
-from apps.mocloud.mocloud_template import DEFAULT_SMS_VERTIFICATOIN_TEMPLATE, DEFAULT_SMS_NOTIFICATION_TEMPLATE, DEFAULT_SIGN_NAME, read_password_from_file
+from apps.mocloud.mocloud_template import (
+    DEFAULT_SMS_VERTIFICATOIN_TEMPLATE,
+    DEFAULT_SMS_NOTIFICATION_TEMPLATE,
+    DEFAULT_SIGN_NAME,
+    read_password_from_file,
+)
 from ..phone_notifications.exceptions import FailedToSendSMS, FailedToStartVerification
 
 
@@ -15,19 +20,18 @@ logger = logging.getLogger(__name__)
 
 class MOCloudSMS:
     def __init__(self):
-        if os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID']!=None:
-            ALIBABA_CLOUD_ACCESS_KEY_ID=os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID']
-        else:
-            ALIBABA_CLOUD_ACCESS_KEY_ID = read_password_from_file(
-                os.environ['ACCESS_KEY_FILE'])
-        if os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET']!=None:
-            ALIBABA_CLOUD_ACCESS_KEY_SECRET=os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET']
-        else:
-            ALIBABA_CLOUD_ACCESS_KEY_SECRET = read_password_from_file(
-                os.environ['SECRET_KEY_FILE'])
 
-        self.sms_client = MOCloudSMS.create_client(
-            ALIBABA_CLOUD_ACCESS_KEY_ID, ALIBABA_CLOUD_ACCESS_KEY_SECRET)
+        if os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID") != None:
+            ALIBABA_CLOUD_ACCESS_KEY_ID = os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID")
+        else:
+            ALIBABA_CLOUD_ACCESS_KEY_ID = read_password_from_file(os.environ.get("ACCESS_KEY_FILE"))
+
+        if os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET") != None:
+            ALIBABA_CLOUD_ACCESS_KEY_SECRET = os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+        else:
+            ALIBABA_CLOUD_ACCESS_KEY_SECRET = read_password_from_file(os.environ.get("SECRET_KEY_FILE"))
+
+        self.sms_client = MOCloudSMS.create_client(ALIBABA_CLOUD_ACCESS_KEY_ID, ALIBABA_CLOUD_ACCESS_KEY_SECRET)
 
     @staticmethod
     def create_client(
@@ -40,7 +44,7 @@ class MOCloudSMS:
             # 必填，您的 AccessKey Secret,
             access_key_secret=access_key_secret,
             # Endpoint 请参考 https://api.aliyun.com/product/Dysmsapi
-            endpoint=f'dysmsapi.aliyuncs.com'
+            endpoint=f"dysmsapi.aliyuncs.com",
         )
         return Dysmsapi20170525Client(config)
 
@@ -49,18 +53,15 @@ class MOCloudSMS:
             phone_numbers=number,
             sign_name=DEFAULT_SIGN_NAME,
             template_code=DEFAULT_SMS_NOTIFICATION_TEMPLATE,
-            template_param=parmas
+            template_param=parmas,
         )
         try:
             # 复制代码运行请自行打印 API 的返回值
-            resp = self.sms_client.send_sms_with_options(
-                send_sms_request, util_models.RuntimeOptions())
+            resp = self.sms_client.send_sms_with_options(send_sms_request, util_models.RuntimeOptions())
             print(f"use tmpl:{send_sms_request.template_code} received code:{resp.body.code}, msg:{resp.body.message}")
-            logger.info(
-                f"send sms notification [{parmas}] to [{number}] success")
+            logger.info(f"send sms notification [{parmas}] to [{number}] success")
         except Exception as error:
-            logger.error(
-                f"send sms notification [{parmas}] to [{number}] failed")
+            logger.error(f"send sms notification [{parmas}] to [{number}] failed")
             # 错误 message
             print(error.message)
             # 诊断地址
@@ -75,17 +76,14 @@ class MOCloudSMS:
             phone_numbers=number,
             sign_name=DEFAULT_SIGN_NAME,
             template_code=DEFAULT_SMS_VERTIFICATOIN_TEMPLATE,
-            template_param='{"code":"%s"}' % (code)
+            template_param='{"code":"%s"}' % (code),
         )
         runtime = util_models.RuntimeOptions()
         try:
-            resp = self.sms_client.send_sms_with_options(
-                send_sms_request, runtime)
-            logger.info(
-                f"send verification code [{code}] to [{number}] success")
+            resp = self.sms_client.send_sms_with_options(send_sms_request, runtime)
+            logger.info(f"send verification code [{code}] to [{number}] success")
         except Exception as error:
-            logger.error(
-                f"send verification code [{code}] to [{number}] failed")
+            logger.error(f"send verification code [{code}] to [{number}] failed")
             # 错误 message
             print(error.message)
             # 诊断地址
--- engine/apps/mocloud/mocloud_vms.py
+++ engine/apps/mocloud/mocloud_vms.py
@@ -1,27 +1,27 @@
 import os
+import logging
 
 from alibabacloud_dyvmsapi20170525.client import Client as Dyvmsapi20170525Client
 from alibabacloud_tea_openapi import models as open_api_models
 from alibabacloud_dyvmsapi20170525 import models as dyvmsapi_20170525_models
 from alibabacloud_tea_util import models as util_models
 from alibabacloud_tea_util.client import Client as UtilClient
-from apps.mocloud.mocloud_template import DEFAULT_VMS_ALERT_TEMPLATE,read_password_from_file
+from apps.mocloud.mocloud_template import DEFAULT_VMS_ALERT_TEMPLATE, read_password_from_file
 
 
 class MOCloudVMS:
     def __init__(self):
-        if os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID']!=None:
-            ALIBABA_CLOUD_ACCESS_KEY_ID=os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID']
+        if os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID") != None:
+            ALIBABA_CLOUD_ACCESS_KEY_ID = os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID")
         else:
-            ALIBABA_CLOUD_ACCESS_KEY_ID = read_password_from_file(
-                os.environ['ACCESS_KEY_FILE'])
-        if os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET']!=None:
-            ALIBABA_CLOUD_ACCESS_KEY_SECRET=os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET']
+            ALIBABA_CLOUD_ACCESS_KEY_ID = read_password_from_file(os.environ.get("ACCESS_KEY_FILE"))
+
+        if os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET") != None:
+            ALIBABA_CLOUD_ACCESS_KEY_SECRET = os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
         else:
-            ALIBABA_CLOUD_ACCESS_KEY_SECRET = read_password_from_file(
-                os.environ['SECRET_KEY_FILE'])
-        self.vms_client = MOCloudVMS.create_client(
-            ALIBABA_CLOUD_ACCESS_KEY_ID, ALIBABA_CLOUD_ACCESS_KEY_SECRET)
+            ALIBABA_CLOUD_ACCESS_KEY_SECRET = read_password_from_file(os.environ.get("SECRET_KEY_FILE"))
+        
+        self.vms_client = MOCloudVMS.create_client(ALIBABA_CLOUD_ACCESS_KEY_ID, ALIBABA_CLOUD_ACCESS_KEY_SECRET)
 
     @staticmethod
     def create_client(
@@ -39,10 +39,10 @@ class MOCloudVMS:
             # 必填，您的 AccessKey ID,
             access_key_id=access_key_id,
             # 必填，您的 AccessKey Secret,
-            access_key_secret=access_key_secret
+            access_key_secret=access_key_secret,
         )
         # Endpoint 请参考 https://api.aliyun.com/product/Dyvmsapi
-        config.endpoint = f'dyvmsapi.aliyuncs.com'
+        config.endpoint = f"dyvmsapi.aliyuncs.com"
         return Dyvmsapi20170525Client(config)
 
     def send_vms_notification(self, number, parmas):
@@ -52,12 +52,11 @@ class MOCloudVMS:
             play_times=3,
             called_number=number,
             tts_code=DEFAULT_VMS_ALERT_TEMPLATE,
-            volume=100
+            volume=100,
         )
         try:
             # 复制代码运行请自行打印 API 的返回值
-            self.vms_client.single_call_by_tts_with_options(
-                single_call_by_voice_request, util_models.RuntimeOptions())
+            self.vms_client.single_call_by_tts_with_options(single_call_by_voice_request, util_models.RuntimeOptions())
         except Exception as error:
             # 错误 message
             # print(error.message)
